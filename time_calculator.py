ddf9dba18e8c943f6d097a14496f39066d917b88
MaCeleste <mceleste.orona@gmail.com>
2022-09-27 19:00:47 +0100

    Final solution, all tests passed. Not very pythonic and could be re written to be more efficient but the logic is correct and it produces the expected result. Will review it after finishing more complex projects.

--- time_calculator.py
+++ time_calculator.py
@@ -1,6 +1,5 @@
 import re
-
-def add_time(start, duration):
+def add_time(start, duration, day=None):
 
   #convert start time and duration to seconds
   startTime = re.split(':| ', start)
@@ -17,10 +16,12 @@ def add_time(start, duration):
   #convert time in seconds to hh:mm:ss and calculate number of days
 
   endHour = endTimeSeconds // 3600
-  if endHour % 24 != 0:
+  numberOfDays = 0
+  if endHour // 24 > 0:
     numberOfDays = endHour // 24
     endHour = endHour - (numberOfDays*24)
   endMin = (endTimeSeconds % 3600) // 60
+
 
   #convert min to str and apply format
 
@@ -36,14 +37,57 @@ def add_time(start, duration):
   elif endHour == 12:
     endHour = str(endHour)
     pmOrAm = ' ' + 'PM'
+  elif endHour == 0:
+    endHour = '12'
+    pmOrAm = ' ' + 'AM'
   else:
     endHour = str(endHour)
     pmOrAm = ' ' + 'AM'
 
-  new_time = ''
-  new_time =  endHour + ":" + endMin + pmOrAm
-  #print(formattedTime)
+
+
+  daysOfTheWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+  #concatenate the parts of the string to create the final output and calculate
+  #the day of the week if a third argument is provided
+  if day:
+    day = day.capitalize()
+    if numberOfDays == 0:
+      newDay = day
+      new_time =  endHour + ":" + endMin + pmOrAm + ', ' + newDay
+    if numberOfDays != 0:
+      if numberOfDays == 1:
+        indexOfDay = daysOfTheWeek.index(day) + 1
+        newDay = ', ' + daysOfTheWeek[indexOfDay]
+        numberOfDays = ' (next day)'
+      else:
+        indexOfDay = (daysOfTheWeek.index(day)+ numberOfDays) % 7
+        newDay = ', ' + daysOfTheWeek[indexOfDay]
+        numberOfDays = ' (' + str(numberOfDays) + ' days later)'
+      new_time =  endHour + ":" + endMin + pmOrAm + newDay + numberOfDays
+  else:
+    if numberOfDays == 0:
+      new_time =  endHour + ":" + endMin + pmOrAm
+    if numberOfDays != 0:
+      if numberOfDays == 1:
+        numberOfDays = ' (next day)'
+      else:
+        numberOfDays = ' (' + str(numberOfDays) + ' days later)'
+      new_time =  endHour + ":" + endMin + pmOrAm + numberOfDays
+
+
+
 
   return new_time
 
 print(add_time("11:40 AM", "0:25"))
+print(add_time("3:30 PM", "2:12"))
+print(add_time("11:55 AM", "3:12"))
+print(add_time("9:15 PM", "5:30"))
+print(add_time("2:59 AM", "24:00"))
+print(add_time("8:16 PM", "466:02"))
+print(add_time("5:01 AM", "0:00"))
+print(add_time("3:30 PM", "2:12", "Monday"))
+print(add_time("2:59 AM", "24:00", "saturDay"))
+print(add_time("11:59 PM", "24:05", "Wednesday"))
+print(add_time("8:16 PM", "466:02", "tuesday"))
